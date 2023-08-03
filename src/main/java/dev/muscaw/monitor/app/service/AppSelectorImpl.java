fbe2da69ce72adff58ae30310c9d602a4112419a
Kevin Grandjean <Muscaw@users.noreply.github.com>
2023-08-03 20:53:55 +0200

    Format

--- src/main/java/dev/muscaw/monitor/app/service/AppSelectorImpl.java
+++ src/main/java/dev/muscaw/monitor/app/service/AppSelectorImpl.java
@@ -3,39 +3,38 @@ package dev.muscaw.monitor.app.service;
 import dev.muscaw.monitor.app.domain.AppSelector;
 import dev.muscaw.monitor.app.domain.AppService;
 import dev.muscaw.monitor.app.domain.NoMatchingAppException;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppSelectorImpl implements AppSelector {
 
-    private List<AppService> services;
+  private List<AppService> services;
 
-    @Autowired
-    public void setServices(List<AppService> services) {
-        var distinctNamesCount = services.stream().map(AppService::getName).distinct().count();
-        if (distinctNamesCount != services.size()) {
-            Set<String> uniqueItems = new HashSet<>();
-            String duplicates = services.stream()
-                    .map(AppService::getName)
-                    .filter(name -> !uniqueItems.add(name))
-                    .collect(Collectors.joining(", "));
-            throw new RuntimeException("Multiple AppService have the same name: " + duplicates);
-        }
-        this.services = services;
+  @Autowired
+  public void setServices(List<AppService> services) {
+    var distinctNamesCount = services.stream().map(AppService::getName).distinct().count();
+    if (distinctNamesCount != services.size()) {
+      Set<String> uniqueItems = new HashSet<>();
+      String duplicates =
+          services.stream()
+              .map(AppService::getName)
+              .filter(name -> !uniqueItems.add(name))
+              .collect(Collectors.joining(", "));
+      throw new RuntimeException("Multiple AppService have the same name: " + duplicates);
     }
+    this.services = services;
+  }
 
-    @Override
-    public AppService selectApp(String name) {
-        return services.stream()
-                .filter(i -> i.getName().equals(name))
-                .findFirst()
-                .orElseThrow(() -> new NoMatchingAppException(name));
-    }
+  @Override
+  public AppService selectApp(String name) {
+    return services.stream()
+        .filter(i -> i.getName().equals(name))
+        .findFirst()
+        .orElseThrow(() -> new NoMatchingAppException(name));
+  }
 }
--- src/test/java/dev/muscaw/monitor/app/service/AppSelectorImplTest.java
+++ src/test/java/dev/muscaw/monitor/app/service/AppSelectorImplTest.java
@@ -1,58 +1,62 @@
 package dev.muscaw.monitor.app.service;
 
-import dev.muscaw.monitor.app.domain.AppService;
-import dev.muscaw.monitor.app.domain.NoMatchingAppException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import dev.muscaw.monitor.app.domain.AppService;
+import dev.muscaw.monitor.app.domain.NoMatchingAppException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AppSelectorImplTest {
 
-    private AppService service1;
-    private AppService service2;
-    private AppService service3;
-    private AppSelectorImpl appSelector;
+  private AppService service1;
+  private AppService service2;
+  private AppService service3;
+  private AppSelectorImpl appSelector;
 
-    private AppService mockAppService(String name) {
-        AppService s = mock(AppService.class);
-        when(s.getName()).thenReturn(name);
-        return s;
-    }
+  private AppService mockAppService(String name) {
+    AppService s = mock(AppService.class);
+    when(s.getName()).thenReturn(name);
+    return s;
+  }
 
-    @BeforeEach
-    public void setUp() {
-        service1 = mockAppService("service1");
-        service2 = mockAppService("service2");
-        service3 = mockAppService("service3");
-        appSelector = new AppSelectorImpl();
-        appSelector.setServices(List.of(service1, service2, service3));
-    }
+  @BeforeEach
+  public void setUp() {
+    service1 = mockAppService("service1");
+    service2 = mockAppService("service2");
+    service3 = mockAppService("service3");
+    appSelector = new AppSelectorImpl();
+    appSelector.setServices(List.of(service1, service2, service3));
+  }
 
-    @Test
-    public void setServices_rejectsDuplicateServices() {
-        var service1Bis = mockAppService("service1");
-        var service2Bis = mockAppService("service2");
-        RuntimeException ex = assertThrows(RuntimeException.class, () -> appSelector.setServices(List.of(service1Bis, service1, service2Bis, service2, service3)));
-        assertEquals("Multiple AppService have the same name: service1, service2", ex.getMessage());
-    }
+  @Test
+  public void setServices_rejectsDuplicateServices() {
+    var service1Bis = mockAppService("service1");
+    var service2Bis = mockAppService("service2");
+    RuntimeException ex =
+        assertThrows(
+            RuntimeException.class,
+            () ->
+                appSelector.setServices(
+                    List.of(service1Bis, service1, service2Bis, service2, service3)));
+    assertEquals("Multiple AppService have the same name: service1, service2", ex.getMessage());
+  }
 
-    @Test
-    public void selectApp_success() {
-        assertEquals(service1, appSelector.selectApp("service1"));
-        assertEquals(service2, appSelector.selectApp("service2"));
-        assertEquals(service3, appSelector.selectApp("service3"));
-    }
+  @Test
+  public void selectApp_success() {
+    assertEquals(service1, appSelector.selectApp("service1"));
+    assertEquals(service2, appSelector.selectApp("service2"));
+    assertEquals(service3, appSelector.selectApp("service3"));
+  }
 
-    @Test
-    public void selectApp_notFound() {
-        NoMatchingAppException ex = assertThrows(NoMatchingAppException.class, () -> appSelector.selectApp("unknown-app"));
-        assertEquals("Could not find app unknown-app", ex.getMessage());
-    }
+  @Test
+  public void selectApp_notFound() {
+    NoMatchingAppException ex =
+        assertThrows(NoMatchingAppException.class, () -> appSelector.selectApp("unknown-app"));
+    assertEquals("Could not find app unknown-app", ex.getMessage());
+  }
 }
