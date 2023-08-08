748512901ac6b4fcf2a783f0e7bac21724c2bbf2
Kevin Grandjean <Muscaw@users.noreply.github.com>
2023-08-08 21:30:23 +0200

    Add tests for monitor app

--- src/main/java/dev/muscaw/monitor/conf/WeatherConfiguration.java
+++ src/main/java/dev/muscaw/monitor/conf/WeatherConfiguration.java
@@ -2,6 +2,7 @@ package dev.muscaw.monitor.conf;
 
 import static dev.muscaw.monitor.conf.Env.getEnv;
 
+import dev.muscaw.monitor.util.domain.LatLon;
 import dev.muscaw.monitor.weather.api.WeatherApiWeatherServiceImpl;
 import dev.muscaw.monitor.weather.domain.WeatherService;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +16,13 @@ public class WeatherConfiguration {
     String token = getEnv("WEATHER_API_TOKEN").orElseThrow();
     String baseUrl = getEnv("WEATHER_API_BASEURL").orElse("https://api.weatherapi.com");
     return new WeatherApiWeatherServiceImpl(baseUrl, token);
+  }
+
+  @Bean(name = "weatherLocation")
+  public LatLon getWeatherLocation() {
+    String latLon = getEnv("WEATHER_LAT_LON").orElseThrow();
+    String[] values = latLon.split(",");
+
+    return new LatLon(Float.parseFloat(values[0]), Float.parseFloat(values[1]));
   }
 }
